4d378e7dea61bdf8e1516de58102e430f751eaad
Nghiem Thanh Pham <nghiempt.dev@gmail.com>
2026-06-05 10:15:11 +0700

    fix: favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ const inter = Inter({
 });
 
 export const metadata: Metadata = {
-  title: "Danmotion",
+  title: "Danmotion LMS",
   description: "Khóa học đầu tiên tại Việt Nam tập trung vào style edit kiểu Apple & Devin Jatho — dạng video đang được nhiều khách hàng quốc tế trả giá cao.",
 };
 
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import Login from "../components/Login";
 
 export const metadata: Metadata = {
-  title: "Danmotion",
+  title: "Danmotion LMS",
   description: "Khóa học đầu tiên tại Việt Nam tập trung vào style edit kiểu Apple & Devin Jatho — dạng video đang được nhiều khách hàng quốc tế trả giá cao.",
 };
 
